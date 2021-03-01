28f3afb9ac33bd2ec4d0ad122d28ff80826e2553
batmanwannaB <36299313+batmanwannaB@users.noreply.github.com>
2021-03-01 11:22:08 +0530

    Add files via upload

---
+++ src/main/scala/Add.scala
@@ -0,0 +1,7 @@
+package add2numbers
+
+class Add {
+  def add( a:Int, b:Int) :Int={
+    return a+b
+  }
+}
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,10 @@
+import add2numbers.Add
+
+object Test {
+
+    @throws[Exception]
+    def main(args: Array[String]): Unit = {
+      println(new Add().add(5,1))
+    }
+}
+class Test{}
